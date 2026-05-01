bb28dab4d346565b9f085971ef17bff7a515d11d
Vinicius Gabriel S. <vinicius78gabriel@gmail.com>
2026-05-01 12:16:21 -0300

    Implementando insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,7 +30,11 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-
+		
+		System.out.println("\n=== Tete 4: seller insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new  Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
