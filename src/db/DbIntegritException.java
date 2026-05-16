a6cbbd49975b3b8f6dfa14652a577d95e43de799
Vinicius Gabriel S. <vinicius78gabriel@gmail.com>
2026-05-16 12:11:08 -0300

    Implementação da classe Department

--- src/db/DbIntegritException.java
+++ src/db/DbIntegritException.java
@@ -5,6 +5,6 @@ public class DbIntegritException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
 	public DbIntegritException(String msg) {
-		super("Não é possível deletar dados que utilizam a mesma chave para integração ou seja, não pode excluir um departamento onde contenha funcionários inseridos");
+		super(msg);
 	}
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -63,5 +63,4 @@ public class Department implements Serializable{
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
 	
-	
 }
